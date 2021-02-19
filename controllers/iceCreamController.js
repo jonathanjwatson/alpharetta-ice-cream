c71e7ded2e62bbb0f2bb7575369f4908ea1120c0
Jonathan J. Watson <jonathanwatson1@gmail.com>
2021-02-18 20:06:57 -0500

    added dotenv for config vars and hard coded api call to yelp

--- controllers/iceCreamController.js
+++ controllers/iceCreamController.js
@@ -1,7 +1,19 @@
+const axios = require("axios");
+
 module.exports = {
   findAll: (req, res) => {
-    res.json({
-      iceCream: [],
-    });
+    // FIXME: convert this to dynamic input
+    axios
+      .get(
+        "https://api.yelp.com/v3/businesses/search?term=ice%20cream&location=Alpharetta&limit=5&sort_by=rating",
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+          },
+        }
+      )
+      .then((response) => {
+        res.json(response.data);
+      });
   },
 };
